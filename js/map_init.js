170dd10424b633e8393c91468c16f637e4d12bfc
U-brandon-PC\brandon <brandon@brandon-PC.(none)>
2011-05-06 20:39:11 -0700

    Initial checkin

---
+++ js/map_init.js
@@ -0,0 +1,52 @@
+window.showzi = {};
+var initialLocation;
+var siberia = new google.maps.LatLng(60, 105);
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var browserSupportFlag =  new Boolean();
+var log=function(message){if(console)console.log(message)};
+dojo.addOnLoad(initialize);
+
+
+function initialize() {
+  var myOptions = {
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  showzi.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  showzi.map.getCenter();
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      showzi.map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  // Try Google Gears Geolocation
+  } else if (google.gears) {
+    browserSupportFlag = true;
+    var geo = google.gears.factory.create('beta.geolocation');
+    geo.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+      showzi.map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeoLocation(browserSupportFlag);
+    });
+  // Browser doesn't support Geolocation
+  } else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = siberia;
+    }
+    showzi.map.setCenter(initialLocation);
+  }
+}
---
+++ js/util.js
@@ -0,0 +1,266 @@
+dojo.require('dojo.io.script');
+dojo.require('dijit.Menu');
+dojo.require('dijit.form.Button');
+dojo.require('dijit.ProgressBar');
+dojo.require('dojo.parser');
+dojo.require('dojo.fx');
+dojo.require('dijit.Tooltip');
+dojo.require('dojox.NodeList.delegate');
+dojo.require('dijit.Dialog');
+dojo.declare('showzi.util.eventful',
+  null, {
+    constructor: function() {
+      this.apiKey = '7pfDQXSvjB7GFm9M';
+      this.category = {};
+      this.categoryButton = null;
+      this.loadingProgress = 0;
+      this.loadingToggler = new dojo.fx.Toggler({
+         node: "downloadProgress"
+      });
+      this.searchParams = {
+        url: 'http://api.eventful.com/json/events/search/',
+        within: null,
+        units: 'mile',
+        location: null,
+        app_key: this.apiKey,
+        date: 'This Week,Next Week',
+        page_size: 400,
+        c: 'music',
+        sort_order: 'popularity'
+        //q: "film"
+      };
+    },
+    setMapParams: function(lat, lng, radius) {
+      this.searchParams.within = radius || 10;
+      var lat = lat || 34.07996230865873;
+      var lng = lng || -118.33648681640625;
+      this.searchParams.location = lat + "," + lng; 
+    },
+    showLoading: function() {
+        showzi.util.eventful.loadingToggler.show();
+        if (showzi.util.eventful.loadingProgress >= 20) {
+          showzi.util.eventful.loadingProgress = 0;
+          showzi.util.eventful.loadingToggler.hide();
+          return;
+        }
+        jsProgress.update({
+            maximum: 20,
+            progress: ++showzi.util.eventful.loadingProgress,
+            showFunc: dojo.fx.wipeIn,
+            hideFunc: dojo.fx.wipeOut
+        });
+        if (showzi.util.eventful.loadingProgress <= 20) {
+            setTimeout(showzi.util.eventful.showLoading, 100);
+        }
+    },
+    setCategory: function(categoryName, categoryId) {
+      this.category.name = categoryName;
+      this.category.id = categoryId;
+      this.categoryButton.set({label : categoryName});
+      this.categoryButton.set({categoryId : categoryId});
+      this.searchParams.c = categoryId;
+      this.searchByCoords();
+    },
+    searchByCoords: function(lat, lng, radius) {
+      this.showLoading();
+      showzi.util.events.list.clear();
+      showzi.clearEvents();
+      this.setMapParams(lat, lng, radius);
+      // workaround note: eventful jsonp api does not like . in the callback name and dojo generates a stub callback method with a dot in it, so a 'gasp' global function must be used
+      var cbName = "cb_searchByCoords_" + (new Date()).getTime();
+      var load = function(data) {
+        showzi.putEvents(data);
+      };
+      var error = function(error) {
+        alert("Danger Will Robinson!");
+      };
+      window[cbName] = load;
+      this.searchParams.callback = cbName;
+      var url = showzi.buildUrl(this.searchParams);
+      dojo.io.script.attach(cbName, url);
+    },
+    getCategories: function() {
+      var cbName = "cb_getCategories_" + (new Date()).getTime();
+      var params = {
+        url: "http://api.eventful.com/json/categories/list/",
+        app_key: this.apiKey,
+        callback: cbName
+      };
+      var load = function(data) {
+        var sMarkup = [];
+        var cMenu = new dijit.Menu({
+          style: "display: none;"
+        });
+        for (var i=0;i<data.category.length;i++) {
+          cMenu.addChild(new dijit.MenuItem({
+            label: data.category[i].name,
+            categoryId: data.category[i].id,
+            onClick: function(e) {
+              showzi.util.eventful.setCategory(this.label, this.categoryId);
+            }
+          }));
+        };
+        showzi.util.eventful.categoryButton = new dijit.form.DropDownButton({
+            label: "Concerts &amp; Tour Dates",
+            name: "programmatic2",
+            dropDown: cMenu,
+            id: "category-list"
+        });
+        dojo.byId("category-list").appendChild(showzi.util.eventful.categoryButton.domNode);
+      };
+      window[cbName] = load;
+      var url = showzi.buildUrl(params);
+      dojo.io.script.attach(cbName, url);
+    }
+  }
+);
+
+dojo.addOnLoad(function() {
+  showzi.util.eventful = new showzi.util.eventful();
+  showzi.util.eventful.searchByCoords();
+  showzi.util.eventful.getCategories();
+});
+
+showzi.buildUrl = function(params) {
+  var firstParamDeclared = false;
+  var sUrl = "";
+  for (idx in params) {
+    if (idx === "url") {
+      sUrl = params[idx] + sUrl;
+    }
+    else if (firstParamDeclared === false) {
+      sUrl = sUrl + "?" + idx + "=" + encodeURIComponent(params[idx]);
+      firstParamDeclared = true;
+    }
+    else {
+      sUrl = sUrl + "&" + idx + "=" + encodeURIComponent(params[idx]);
+    }
+  }
+  return sUrl;
+}
+
+showzi.clearEvents = function() {
+  if (showzi.eventMarkers) {
+    var offset = 0;
+    for (var i=showzi.eventMarkers.length - 1; i>=0;i--) {
+      showzi.eventMarkers[i].setMap(null);
+      // we want to keep their favorites, don't delete - some conditional here
+      // if (condition) {} else 
+      showzi.eventMarkers.splice(i, 1);
+      // also, cool to use persistent store for their favorites!
+    }
+    console.log(showzi.eventMarkers);
+  }
+}
+
+showzi.eventMarkers = [];
+
+showzi.putEvents = function(data) {
+  for (idx in data.events.event) {
+    var event = data.events.event[idx];
+    var categoryId = showzi.util.eventful.category.id;
+    var categoryName = showzi.util.eventful.category.name;  
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(event.latitude, event.longitude), 
+      map: showzi.map,
+      title: event.title,
+      event: event, 
+      category: {
+        name: categoryName,
+        id: categoryId
+      }
+    });
+    showzi.eventMarkers.push(marker);
+    google.maps.event.addListener(marker, 'click',  function() {
+      console.log(event);
+      showzi.loadWidget(event);
+    });    
+  }
+  showzi.util.events.list.render();
+}
+
+showzi.util.events = {
+  initialized: false, 
+  init: function() {
+    if (!showzi.util.events.initialized) {
+      dojo.query('#event_list').delegate('img.info', 'onclick', function(evt) {
+        var eventMarkerIdx = dojo.query(evt.target).parents('.event_item')[0].id.split('_')[2];
+        new dijit.Dialog({
+          title: showzi.eventMarkers[eventMarkerIdx].event.title,
+          style: 'width: 500px',
+          content: showzi.eventMarkers[eventMarkerIdx].event.description
+        }).show();
+        // TODO: tear down the dialog
+      });
+    }
+    showzi.util.events.initialized = true;
+  },
+  list: {
+    render : function() {
+      showzi.util.events.init();
+      var markup = [];
+      for (var eventMarker in showzi.eventMarkers) {
+        var event = showzi.eventMarkers[eventMarker].event; 
+        markup.push(showzi.util.events.item.render(event, eventMarker));
+      }
+      dojo.byId('event_list').innerHTML = markup.join('');
+      dojo.query('#event_list').delegate('img.target', 'onclick', function(evt) {
+        var eventMarkerIdx = dojo.query(evt.target).parents('.event_item')[0].id.split('_')[2];
+        showzi.map.setCenter(new google.maps.LatLng(showzi.eventMarkers[eventMarkerIdx].event.latitude, showzi.eventMarkers[eventMarkerIdx].event.longitude));
+        showzi.eventMarkers[eventMarkerIdx].setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout('showzi.eventMarkers[' + eventMarkerIdx + '].setAnimation(null)', 1500);
+      });
+    },
+    clear : function() {
+      dojo.byId('event_list').innerHTML = '';
+    }
+  },
+  item: {
+    render : function(event, index) {
+      var markup = [];
+      markup.push(['<div class="event_item" id="event_item_',index,'"><div class="image_title">'].join(''));
+      if (event.image && event.image.small) {
+        markup.push(['<img class="image" src="',event.image.small.url,'">'].join(''));
+      }
+      else {
+        markup.push(['<img class="image" src="/images/fail.jpg">'].join(''));
+      }
+      markup.push(['<div class="title">',event.title,'</div>'].join(''));
+      markup.push(['<div class="icons"><img class="target" src="/images/target.png"><img class="info" src="/images/info.jpg"></div>'].join(''));
+      //markup.push(['<div class="description">',event.description,'</div>'].join(''));
+      markup.push(['</div><div class="location">',event.city_name,', ',event.region_abbr,'</div>'].join(''));
+      markup.push(['<div class="start_time">',new Date(event.start_time).getShortDate(),'</div>'].join(''));
+      if (event.stop_time && false) {
+        markup.push(['<div class="stop_time">',new Date(event.stop_time).getShortDate(),'</div>'].join(''));
+      }
+      markup.push(['<div class="venue_name">',event.venue_name,'</div>'].join(''));
+      markup.push(['<div class="venue_address">',event.venue_address,'</div>'].join(''));
+      markup.push('</div>');
+      return markup.join('');
+/*
+city_name: "Los Angeles"
+country_name: "United States"
+description: "LUNAFEST, a traveling film festival by, for, about women is dedicated to building community,through …"
+geocode_type: "EVDB Geocoder"
+id: "E0-001-037403492-9"
+image: null
+region_name: "California"
+start_time: "2011-05-10 08:30:00"
+stop_time: null
+title: "LUNAFEST @ Professional BusinessWomen of California 22nd Annual Conference"
+venue_address: "747 Howard St."
+venue_name: "Moscone Center South"
+venue_url: "http://eventful.com/losangeles/venues/moscone-center-south-/V0-001-003996807-7"
+watching_count: null
+*/
+    }
+  }
+};
+
+Date.prototype.getShortDate = function(){
+  return this.getMonth() + "/" +  this.getDate() + "/" +  this.getFullYear();
+}
+showzi.loadWidget = function() {
+
+}
+
